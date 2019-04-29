ff97762a56271f8d32854061e1778bebab859ffe
Tony Blanco <tony.blanco@softvision.com>
2019-04-29 11:02:02 -0300

    Question model

--- app/User.php
+++ app/User.php
@@ -36,4 +36,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get questions from user
+     *
+     * @return void
+     */
+    public function questions(){
+      
+      return $this->hasMany(Question::class);
+    }
+
+    /**
+     * Set title and slug attribute
+     * 
+     */
+    public function setTitleAttribute($value) {
+
+      $this->attributes['title'] = $value;
+      $this->attributes['slug'] = str_slug($value);
+    }
 }
