b0ddcc35c6c688e7921611411f17094b7dcbfb71
Tony Blanco <tony.blanco@softvision.com>
2019-05-01 19:05:57 -0300

    Store and flash message feature

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -4,15 +4,18 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                  <div class="d-flex align-items-center">
-                    <h2> All Questions </h2>
-                    <div class="ml-auto">
-                      <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask a Question</a>
-                    </div>
-                  </div>
+
+          
+          <div class="card">
+            <div class="card-header">
+              <div class="d-flex align-items-center">
+                <h2> All Questions </h2>
+                <div class="ml-auto">
+                  <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask a Question</a>
                 </div>
+              </div>
+            </div>
+            @include('layouts._messages')
 
                 <div class="card-body">
                    @foreach ($questions as $question)
